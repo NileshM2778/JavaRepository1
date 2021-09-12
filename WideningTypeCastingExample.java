f3f321887a347cd4602b456354fd453f790fc4ee
NileshM2778 <90493676+NileshM2778@users.noreply.github.com>
2021-09-12 19:35:48 +0530

    Add files via upload

---
+++ WideningTypeCastingExample.java
@@ -0,0 +1,14 @@
+package MyPack;
+
+public class WideningTypeCastingExample 
+{
+	public static void main(String args[])
+	{
+		int i=10;
+		double d=i;
+		System.out.println("Converting int into double "+d);
+		long l=i;
+		System.out.println("Converting int into log "+l);
+		
+	}
+}
